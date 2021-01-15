c29ae9bbb3ad911ed412781e9b893a667e2416e9
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2021-01-15 01:03:51 -0300

    Add files via upload

---
+++ BitaBitInterface/BitaBitInterface/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿
+namespace projBitABit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblBit4 = new System.Windows.Forms.Label();
+            this.lblBit3 = new System.Windows.Forms.Label();
+            this.lblBit2 = new System.Windows.Forms.Label();
+            this.lblBit1 = new System.Windows.Forms.Label();
+            this.lblBit5 = new System.Windows.Forms.Label();
+            this.lblBit6 = new System.Windows.Forms.Label();
+            this.lblBit7 = new System.Windows.Forms.Label();
+            this.lblBit8 = new System.Windows.Forms.Label();
+            this.txtBit = new System.Windows.Forms.TextBox();
+            this.btnLiga = new System.Windows.Forms.Button();
+            this.btnDesliga = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Personalizar = new System.Windows.Forms.RadioButton();
+            this.LigaGeral = new System.Windows.Forms.RadioButton();
+            this.desligaGeral = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblBit4
+            // 
+            this.lblBit4.AutoSize = true;
+            this.lblBit4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBit4.Location = new System.Drawing.Point(262, 16);
+            this.lblBit4.Name = "lblBit4";
+            this.lblBit4.Size = new System.Drawing.Size(18, 20);
+            this.lblBit4.TabIndex = 0;
+            this.lblBit4.Text = "0";
+            // 
+            // lblBit3
+            // 
+            this.lblBit3.AutoSize = true;
+            this.lblBit3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBit3.Location = new System.Drawing.Point(286, 16);
+            this.lblBit3.Name = "lblBit3";
+            this.lblBit3.Size = new System.Drawing.Size(18, 20);
+            this.lblBit3.TabIndex = 1;
+            this.lblBit3.Text = "0";
+            // 
+            // lblBit2
+            // 
+            this.lblBit2.AutoSize = true;
+            this.lblBit2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBit2.Location = new System.Drawing.Point(310, 16);
+            this.lblBit2.Name = "lblBit2";
+            this.lblBit2.Size = new System.Drawing.Size(18, 20);
+            this.lblBit2.TabIndex = 2;
+            this.lblBit2.Text = "0";
+            // 
+            // lblBit1
+            // 
+            this.lblBit1.AutoSize = true;
+            this.lblBit1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBit1.Location = new System.Drawing.Point(334, 16);
+            this.lblBit1.Name = "lblBit1";
+            this.lblBit1.Size = new System.Drawing.Size(18, 20);
+            this.lblBit1.TabIndex = 3;
+            this.lblBit1.Text = "0";
+            // 
+            // lblBit5
+            // 
+            this.lblBit5.AutoSize = true;
+            this.lblBit5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBit5.Location = new System.Drawing.Point(238, 16);
+            this.lblBit5.Name = "lblBit5";
+            this.lblBit5.Size = new System.Drawing.Size(18, 20);
+            this.lblBit5.TabIndex = 4;
+            this.lblBit5.Text = "0";
+            // 
+            // lblBit6
+            // 
+            this.lblBit6.AutoSize = true;
+            this.lblBit6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBit6.Location = new System.Drawing.Point(214, 16);
+            this.lblBit6.Name = "lblBit6";
+            this.lblBit6.Size = new System.Drawing.Size(18, 20);
+            this.lblBit6.TabIndex = 5;
+            this.lblBit6.Text = "0";
+            // 
+            // lblBit7
+            // 
+            this.lblBit7.AutoSize = true;
+            this.lblBit7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBit7.Location = new System.Drawing.Point(190, 16);
+            this.lblBit7.Name = "lblBit7";
+            this.lblBit7.Size = new System.Drawing.Size(18, 20);
+            this.lblBit7.TabIndex = 6;
+            this.lblBit7.Text = "0";
+            // 
+            // lblBit8
+            // 
+            this.lblBit8.AutoSize = true;
+            this.lblBit8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBit8.Location = new System.Drawing.Point(166, 16);
+            this.lblBit8.Name = "lblBit8";
+            this.lblBit8.Size = new System.Drawing.Size(18, 20);
+            this.lblBit8.TabIndex = 7;
+            this.lblBit8.Text = "0";
+            // 
+            // txtBit
+            // 
+            this.txtBit.Location = new System.Drawing.Point(9, 123);
+            this.txtBit.Name = "txtBit";
+            this.txtBit.Size = new System.Drawing.Size(69, 20);
+            this.txtBit.TabIndex = 12;
+            // 
+            // btnLiga
+            // 
+            this.btnLiga.Location = new System.Drawing.Point(106, 109);
+            this.btnLiga.Name = "btnLiga";
+            this.btnLiga.Size = new System.Drawing.Size(80, 21);
+            this.btnLiga.TabIndex = 17;
+            this.btnLiga.Text = "LIGA";
+            this.btnLiga.UseVisualStyleBackColor = true;
+            this.btnLiga.Click += new System.EventHandler(this.btnLiga_Click);
+            // 
+            // btnDesliga
+            // 
+            this.btnDesliga.Location = new System.Drawing.Point(106, 136);
+            this.btnDesliga.Name = "btnDesliga";
+            this.btnDesliga.Size = new System.Drawing.Size(80, 21);
+            this.btnDesliga.TabIndex = 18;
+            this.btnDesliga.Text = "DESLIGA";
+            this.btnDesliga.UseVisualStyleBackColor = true;
+            this.btnDesliga.Click += new System.EventHandler(this.btnDesliga_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.Personalizar);
+            this.groupBox1.Controls.Add(this.LigaGeral);
+            this.groupBox1.Controls.Add(this.desligaGeral);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(49, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(502, 106);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chave geral";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Valor personalizado";
+            // 
+            // Personalizar
+            // 
+            this.Personalizar.AutoSize = true;
+            this.Personalizar.Location = new System.Drawing.Point(130, 63);
+            this.Personalizar.Name = "Personalizar";
+            this.Personalizar.Size = new System.Drawing.Size(57, 17);
+            this.Personalizar.TabIndex = 20;
+            this.Personalizar.Text = "Aplicar";
+            this.Personalizar.UseVisualStyleBackColor = true;
+            this.Personalizar.CheckedChanged += new System.EventHandler(this.Personalizar_CheckedChanged);
+            // 
+            // LigaGeral
+            // 
+            this.LigaGeral.AutoSize = true;
+            this.LigaGeral.Location = new System.Drawing.Point(409, 52);
+            this.LigaGeral.Name = "LigaGeral";
+            this.LigaGeral.Size = new System.Drawing.Size(48, 17);
+            this.LigaGeral.TabIndex = 1;
+            this.LigaGeral.Text = "Ligar";
+            this.LigaGeral.UseVisualStyleBackColor = true;
+            this.LigaGeral.CheckedChanged += new System.EventHandler(this.LigaGeral_CheckedChanged);
+            // 
+            // desligaGeral
+            // 
+            this.desligaGeral.AutoSize = true;
+            this.desligaGeral.Checked = true;
+            this.desligaGeral.Location = new System.Drawing.Point(409, 22);
+            this.desligaGeral.Name = "desligaGeral";
+            this.desligaGeral.Size = new System.Drawing.Size(63, 17);
+            this.desligaGeral.TabIndex = 0;
+            this.desligaGeral.TabStop = true;
+            this.desligaGeral.Text = "Desligar";
+            this.desligaGeral.UseVisualStyleBackColor = true;
+            this.desligaGeral.CheckedChanged += new System.EventHandler(this.desligaGeral_CheckedChanged_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(214, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "00000000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(406, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "000";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(49, 133);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 95);
+            this.panel1.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(372, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 20);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Hexadecimal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(225, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 20);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Binario";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(45, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 20);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Decimal";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.txtBit);
+            this.groupBox2.Controls.Add(this.btnDesliga);
+            this.groupBox2.Controls.Add(this.btnLiga);
+            this.groupBox2.Controls.Add(this.lblBit8);
+            this.groupBox2.Controls.Add(this.lblBit7);
+            this.groupBox2.Controls.Add(this.lblBit6);
+            this.groupBox2.Controls.Add(this.lblBit5);
+            this.groupBox2.Controls.Add(this.lblBit1);
+            this.groupBox2.Controls.Add(this.lblBit2);
+            this.groupBox2.Controls.Add(this.lblBit3);
+            this.groupBox2.Controls.Add(this.lblBit4);
+            this.groupBox2.Location = new System.Drawing.Point(49, 241);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(500, 177);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Bits específicos";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 107);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "1 - 8";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(653, 430);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+
+        private System.Windows.Forms.Label lblBit4;
+        private System.Windows.Forms.Label lblBit3;
+        private System.Windows.Forms.Label lblBit2;
+        private System.Windows.Forms.Label lblBit1;
+        private System.Windows.Forms.Label lblBit5;
+        private System.Windows.Forms.Label lblBit6;
+        private System.Windows.Forms.Label lblBit7;
+        private System.Windows.Forms.Label lblBit8;
+        private System.Windows.Forms.TextBox txtBit;
+        private System.Windows.Forms.Button btnLiga;
+        private System.Windows.Forms.Button btnDesliga;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton LigaGeral;
+        private System.Windows.Forms.RadioButton desligaGeral;
+        private System.Windows.Forms.RadioButton Personalizar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ BitaBitInterface/BitaBitInterface/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace projBitABit
+{
+    public partial class Form1 : Form
+    {
+        private Bits bits;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bits = this.bits = new Bits();
+        }
+
+        private void btnLiga_Click(object sender, EventArgs e)
+        {
+            bits.setBit1(int.Parse(txtBit.Text));
+            label1.Text = Convert.ToString(bits.getBits());
+            label2.Text = Convert.ToString(bits.getBits(), 2);
+            label3.Text = Convert.ToString(bits.getBits(), 16).ToUpper();
+            lblBit1.Text = (bits.getBits(1)) ? "1" : "0";
+            lblBit2.Text = (bits.getBits(2)) ? "1" : "0";
+            lblBit3.Text = (bits.getBits(3)) ? "1" : "0";
+            lblBit4.Text = (bits.getBits(4)) ? "1" : "0";
+            lblBit5.Text = (bits.getBits(5)) ? "1" : "0";
+            lblBit6.Text = (bits.getBits(6)) ? "1" : "0";
+            lblBit7.Text = (bits.getBits(7)) ? "1" : "0";
+            lblBit8.Text = (bits.getBits(8)) ? "1" : "0";
+        }
+
+        private void btnDesliga_Click(object sender, EventArgs e)
+        {
+            bits.setBit0(int.Parse(txtBit.Text));
+            label1.Text = Convert.ToString(bits.getBits());
+            label2.Text = Convert.ToString(bits.getBits(), 2);
+            label3.Text = Convert.ToString(bits.getBits(), 16).ToUpper();
+            lblBit1.Text = (bits.getBits(1)) ? "1" : "0";
+            lblBit2.Text = (bits.getBits(2)) ? "1" : "0";
+            lblBit3.Text = (bits.getBits(3)) ? "1" : "0";
+            lblBit4.Text = (bits.getBits(4)) ? "1" : "0";
+            lblBit5.Text = (bits.getBits(5)) ? "1" : "0";
+            lblBit6.Text = (bits.getBits(6)) ? "1" : "0";
+            lblBit7.Text = (bits.getBits(7)) ? "1" : "0";
+            lblBit8.Text = (bits.getBits(8)) ? "1" : "0";
+        }
+
+        private void desligaGeral_CheckedChanged_1(object sender, EventArgs e)
+        {
+            string x;
+            x = "0";
+            bits.setBits(byte.Parse(x));
+
+            label1.Text = Convert.ToString(bits.getBits());
+            label2.Text = Convert.ToString(bits.getBits(), 2);
+            label3.Text = Convert.ToString(bits.getBits(), 16).ToUpper();
+            lblBit1.Text = (bits.getBits(1)) ? "1" : "0";
+            lblBit2.Text = (bits.getBits(2)) ? "1" : "0";
+            lblBit3.Text = (bits.getBits(3)) ? "1" : "0";
+            lblBit4.Text = (bits.getBits(4)) ? "1" : "0";
+            lblBit5.Text = (bits.getBits(5)) ? "1" : "0";
+            lblBit6.Text = (bits.getBits(6)) ? "1" : "0";
+            lblBit7.Text = (bits.getBits(7)) ? "1" : "0";
+            lblBit8.Text = (bits.getBits(8)) ? "1" : "0";
+        }
+
+        private void LigaGeral_CheckedChanged(object sender, EventArgs e)
+        {
+            string x;
+            x = "255";
+            bits.setBits(byte.Parse(x));
+
+            label1.Text = Convert.ToString(bits.getBits());
+            label2.Text = Convert.ToString(bits.getBits(), 2);
+            label3.Text = Convert.ToString(bits.getBits(), 16).ToUpper();
+            lblBit1.Text = (bits.getBits(1)) ? "1" : "0";
+            lblBit2.Text = (bits.getBits(2)) ? "1" : "0";
+            lblBit3.Text = (bits.getBits(3)) ? "1" : "0";
+            lblBit4.Text = (bits.getBits(4)) ? "1" : "0";
+            lblBit5.Text = (bits.getBits(5)) ? "1" : "0";
+            lblBit6.Text = (bits.getBits(6)) ? "1" : "0";
+            lblBit7.Text = (bits.getBits(7)) ? "1" : "0";
+            lblBit8.Text = (bits.getBits(8)) ? "1" : "0";
+        }
+
+        private void Personalizar_CheckedChanged(object sender, EventArgs e)
+        {
+            bits.setBits(byte.Parse(textBox1.Text));
+            label1.Text = Convert.ToString(bits.getBits());
+            label2.Text = Convert.ToString(bits.getBits(), 2);
+            label3.Text = Convert.ToString(bits.getBits(), 16).ToUpper();
+            lblBit1.Text = (bits.getBits(1)) ? "1" : "0";
+            lblBit2.Text = (bits.getBits(2)) ? "1" : "0";
+            lblBit3.Text = (bits.getBits(3)) ? "1" : "0";
+            lblBit4.Text = (bits.getBits(4)) ? "1" : "0";
+            lblBit5.Text = (bits.getBits(5)) ? "1" : "0";
+            lblBit6.Text = (bits.getBits(6)) ? "1" : "0";
+            lblBit7.Text = (bits.getBits(7)) ? "1" : "0";
+            lblBit8.Text = (bits.getBits(8)) ? "1" : "0";
+        }
+    }
+}
